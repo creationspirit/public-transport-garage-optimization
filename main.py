0cb19838fbaf48de9bf99f2e338d7f6c4b7c9ed6
Andrija Perusic <aperusic18@gmail.com>
2019-01-02 18:36:18 +0100

    Add instance loading from file, flake8 configuration

--- main.py
+++ main.py
@@ -1,5 +1,68 @@
+import argparse
+
+
 def main():
-    return None
+    parser = argparse.ArgumentParser(
+        description='Optimization of public transport garage schedule.')
+    parser.add_argument('inputfile', metavar='input_file',
+                        help='Name of input file stored in data/ folder')
+
+    args = parser.parse_args()
+    (vehicle_count, track_count, vehicle_lengths, vehicle_series,
+        vehicle_restrictions, track_lengths, departure_times,
+        schedule_type, blocking_tracks) = load_instance(args.inputfile)
+    print(vehicle_count)
+    print(track_count)
+    print(vehicle_lengths)
+    print(vehicle_series)
+    print(vehicle_restrictions)
+    print(track_lengths)
+    print(departure_times)
+    print(schedule_type)
+    print(blocking_tracks)
+
+
+def load_instance(filename):
+    file_path = 'data/{}'.format(filename)
+    with open(file_path, 'r') as f:
+        try:
+            vehicle_count = int(f.readline())
+            track_count = int(f.readline())
+            f.readline()
+
+            vehicle_lengths = [int(x) for x in f.readline().strip().split(' ')]
+            f.readline()
+
+            vehicle_series = [int(x) for x in f.readline().strip().split(' ')]
+            f.readline()
+
+            vehicle_restrictions = []
+            for _ in range(vehicle_count):
+                vehicle_restriction = [True if x == '1' else False
+                                       for x in f.readline().strip().split(' ')]
+                vehicle_restrictions.append(vehicle_restriction)
+            f.readline()
+
+            track_lengths = [int(x) for x in f.readline().strip().split(' ')]
+            f.readline()
+
+            departure_times = [int(x) for x in f.readline().strip().split(' ')]
+            f.readline()
+
+            schedule_type = [int(x) for x in f.readline().strip().split(' ')]
+            f.readline()
+
+            blocking_tracks = {}
+            for line in f.readlines():
+                blocking_record = [int(x) for x in line.strip().split(' ')]
+                blocking_tracks[blocking_record[0]] = blocking_record[1:]
+
+            return (
+                vehicle_count, track_count, vehicle_lengths, vehicle_series, vehicle_restrictions,
+                track_lengths, departure_times, schedule_type, blocking_tracks
+            )
+        except ValueError:
+            raise Exception('Instance input file is incorrectly formatted!')
 
 
 if __name__ == "__main__":
